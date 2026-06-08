49662b5c8847fee5fb56aa90c9e49d3cbd838f05
apsingh650 <apsingh650ig@gmail.com>
2026-06-08 21:28:24 +0530

    updated files

---
+++ DAY8/q32.cpp
@@ -0,0 +1,19 @@
+// Program to print repeated number pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
